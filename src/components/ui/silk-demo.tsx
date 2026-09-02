ce0a7b5c756945177db47a2670004fa29fdb9c45
Shahil-Arijentek <shahil@arijentek.com>
2026-09-02 15:58:50 +0530

    feat: replace static team image with interactive CardStack component and remove obsolete UI components and assets

--- src/components/ui/silk-demo.tsx
+++
@@ -1,5 +0,0 @@
-import { Component as SilkBackground } from "@/components/ui/silk-background-animation";
-
-export default function SilkDemo() {
-  return <SilkBackground showText={true} colorScheme="crimson" />;
-}
